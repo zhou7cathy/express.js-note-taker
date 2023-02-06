1875cd4735703a39b1a6e2d7612bc09bac75b389
unknown <zhou7cathy@gmail.com>
2023-02-06 16:57:15 +0800

    updated api/notes get route and HTML * get route.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const Notes =require('./db/db.json')
 const { v4: uuidv4 } = require('uuid');
 
 const PORT = 3001;
@@ -16,68 +17,66 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET request for notes
+app.get('/api/notes', (req, res) => res.json(Notes));
+
+// POST request to add a notes
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a notes`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    // Obtain existing notes
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new note
+        parsedNotes.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated db!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting notes');
+  }
+});
+
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET request for notes
-app.get('/api/notes', (req, res) => {
-    res.status(200).json(`${req.method} request received to get notes`);
-    console.info(`${req.method} request received to get notes`);
-  });
-  
-  // POST request to add a notes
-  app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a notes`);
-  
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-  
-    // If all the required properties are present
-    if (title && text) {
-      // Variable for the object we will save
-      const newNotes = {
-        title,
-        text,
-        note_id: uuidv4(),
-      };
-  
-      // Obtain existing notes
-      fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          // Convert string into JSON object
-          const parsedNotes = JSON.parse(data);
-  
-          // Add a new note
-          parsedNotes.push(newNotes);
-  
-          // Write updated notes back to the file
-          fs.writeFile(
-            './db/db.json',
-            JSON.stringify(parsedNotes, null, 4),
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully updated db!')
-          );
-        }
-      });
-  
-      const response = {
-        status: 'success',
-        body: newNotes,
-      };
-  
-      console.log(response);
-      res.status(201).json(response);
-    } else {
-      res.status(500).json('Error in posting notes');
-    }
-  });
-  
-  app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT} 🚀`)
-  );
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
   
